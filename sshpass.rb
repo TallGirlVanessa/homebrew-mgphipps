ffec1d4c2696e6bb70628e845ef0c02f98f19671
Matthew Phipps <matthew.g.phipps@ieee.org>
2014-01-20 19:15:38 -0500

    Update sshpass with SHA1

--- sshpass.rb
+++ sshpass.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Sshpass < Formula
   url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.05/sshpass-1.05.tar.gz'
   homepage 'http://sourceforge.net/projects/sshpass'
-  md5 'c52d65fdee0712af6f77eb2b60974ac7'
+  sha1 '6dafec86dd74315913417829542f4023545c8fd7'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
